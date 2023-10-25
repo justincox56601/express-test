2763bc3e7467f86ebdb92fbeaa531fb267f43df7
Justin <justin.cox56601@gamil.com>
2023-10-25 13:22:24 -0500

    added get users routes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// fake super sophisticated 'database table'
+const users = [
+	{_id: 1, name: 'John'},
+	{_id: 2, name: 'Jane'},
+	{_id: 3, name: 'Penny'}
+];
+
 app.listen(3000, ()=>{
 	console.log('server is running on port 3000')
 });
@@ -9,4 +16,38 @@ app.get('/', (req, res)=>{
 	res.json({
 		message: 'Hello World'
 	})
-})
+});
+
+app.get('/users', (req, res)=>{
+	res.json({
+		users 
+	})
+});
+
+app.get('/user-by-id/:id', (req, res)=>{
+	const {id} = req.params
+	if(id == null){
+		res.sendStatus(404);
+	}
+
+	const user = users.find(el => el._id === parseInt(id, 10))
+	if(user != null){
+		res.json({user})
+	}else{
+		res.sendStatus(404);
+	}
+});
+
+app.get('/user-by-name/:name', (req, res)=>{
+	const {name} = req.params;
+	if(name == null){
+		res.sendStatus(404);
+	}
+
+	const user = users.find(el => el.name === name)
+	if(user != null){
+		res.json({user})
+	}else{
+		res.sendStatus(404)
+	}
+});
