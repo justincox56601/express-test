11a80e2963c2e3b4e549bd7af7ef116378085460
Justin <justin.cox56601@gamil.com>
2023-10-25 14:33:50 -0500

    added post end point and very elementary authorization

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // fake super sophisticated 'database table'
 const users = [
 	{_id: 1, name: 'John'},
 	{_id: 2, name: 'Jane'},
 	{_id: 3, name: 'Penny'}
 ];
+const auth = [
+	{_id: 1, user_id: 1, token: 'abc123'}
+]
+
+const getAuth = (token) =>{
+	if(token == null){return false}
+	const isAuthorized = auth.some(el => el.token === token)
+	return isAuthorized;
+}
 
 app.listen(3000, ()=>{
 	console.log('server is running on port 3000')
@@ -19,9 +31,7 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/users', (req, res)=>{
-	res.json({
-		users 
-	})
+	res.status(200).json({users})
 });
 
 app.get('/user-by-id/:id', (req, res)=>{
@@ -32,7 +42,7 @@ app.get('/user-by-id/:id', (req, res)=>{
 
 	const user = users.find(el => el._id === parseInt(id, 10))
 	if(user != null){
-		res.json({user})
+		res.status(200).json({user})
 	}else{
 		res.sendStatus(404);
 	}
@@ -46,8 +56,25 @@ app.get('/user-by-name/:name', (req, res)=>{
 
 	const user = users.find(el => el.name === name)
 	if(user != null){
-		res.json({user})
+		res.status(200).json({user})
 	}else{
 		res.sendStatus(404)
+	}
+});
+
+app.post('/user', (req, res)=>{
+	const isAuthorized = getAuth(req.headers.authorization)
+
+	if(!isAuthorized){
+		res.sendStatus(403)
+	}else{
+		const {name} = req.body;
+		const id = users.length+1;
+		users.push({
+			_id: id,
+			name: name
+		})
+
+		res.status(201).json({users});
 	}
 });
